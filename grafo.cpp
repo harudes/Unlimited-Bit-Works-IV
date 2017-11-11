810a0c362de3387ca4745c1ea7a71c7eb3919726
harudes <luis.rendon@ucsp.edu.pe>
2017-11-11 08:51:21 -0800

    Add files via upload

--- grafo.cpp
+++ grafo.cpp
@@ -154,11 +154,11 @@ public:
 		Node* A;
 		if(findNode(dato,A)){
 			int tam=A->aristas.size();
-			for(int i=tam;i>0;i--){
+			for(int i=tam-1;i>0;i--){
 				removeEdge(A->aristas[i]->nodos[0]->nombre,A->aristas[i]->nodos[1]->nombre,A->aristas[i]->tam,A->aristas[i]->dir);
 			}
-			delete(A);
 			nodos.erase(nodos.begin()+nodeGraphPos(dato));
+			delete(A);
 			return 1;
 		}
 		return 0;
@@ -197,7 +197,7 @@ public:
 				a.push_back(b[i]);
 		}
 	}
-	void recorrer(vector<Path*> &rutas,Node *nodo,Edge *arista){
+	void recorrer(vector<Path*> &rutas,Node *nodo,Edge *arista,vector<Node*> &modificados){
 		Path* temp;
 		Node* b;
 		if(arista->nodos[0]==nodo)
@@ -210,6 +210,8 @@ public:
 		temp->tam=arista->tam+findPath(nodo,rutas)->tam;
 		if(!findPath(b,rutas)){
 			rutas.push_back(temp);
+			if(!findNode2(b,modificados))
+				modificados.push_back(b);
 		}
 		else{
 			Path* ruta=findPath(b,rutas);
@@ -217,43 +219,37 @@ public:
 				if((ruta->tam)>(temp->tam)){
 					ruta->nodos=temp->nodos;
 					ruta->tam=temp->tam;
+					if(!findNode2(b,modificados))
+						modificados.push_back(b);
 				}
 			}
 		}
 	}
-	void dijkstra2(vector<Node*> visitados, vector<Path*> &rutas,Node *start, vector<Node*> &avance){
+	void dijkstra2(vector<Node*> visitados, vector<Path*> &rutas,Node *start){
+		vector<Node*> visitados2;
 		for(int j=0;j<visitados.size();j++){
 			Node* A=visitados[j];
 			for(int i=0;i<A->aristas.size();i++){
 				if(!(A->aristas[i]->dir&&nodePos(A->aristas[i],A))&&A->aristas[i]->nodos[0]!=start &&A->aristas[i]->nodos[1]!=start){
-					recorrer(rutas,A,A->aristas[i]);
-					if(!findNode2(A,avance))
-						avance.push_back(A);
+					recorrer(rutas,A,A->aristas[i],visitados2);
 				}
 			}
 		}
-		sumaVector(avance,visitados);
-		vector<Node*> visitados2;
-		for(int i=1; i<rutas.size();i++)
-			visitados2.push_back(rutas[i]->nodo);
-		restaVector(visitados2,avance);
 		if(visitados2.size()>0)
-			dijkstra2(visitados2,rutas,start,avance);
+			dijkstra2(visitados2,rutas,start);
 	}
 	void dijkstra(n a){
 		Node* A;
 		vector<Path*> rutas;
+		vector<Node*> modificados;
 		if(findNode(a,A)){
 			Path* inicio=new Path(A);
 			rutas.push_back(inicio);
 			for(int i=0;i<A->aristas.size();i++){
 				if(!(A->aristas[i]->dir&&nodePos(A->aristas[i],A)))
-					recorrer(rutas,A,A->aristas[i]);
+					recorrer(rutas,A,A->aristas[i],modificados);
 			}
-			vector<Node*> visitados;
-			for(int i=1; i<rutas.size();i++)
-				visitados.push_back(rutas[i]->nodo);
-			dijkstra2(visitados,rutas,A,visitados);
+			dijkstra2(modificados,rutas,A);
 		}
 		for(int i=1;i<rutas.size();i++){
 			for(int j=0;j<rutas[i]->nodos.size();j++){
@@ -265,21 +261,6 @@ public:
 	}
 };
 
-bool nana2(int p, vector<int> vec){
-	for(int i=0;i<vec.size();i++){
-		if(vec[i]==p)
-			return 1;
-	}
-	return 0;
-}
-
-void nana(vector<int> &a, vector<int> b){
-	for(int i=a.size()-1;i>=0;i--){
-		if(nana2(a[i],b))
-			a.erase(a.begin()+i);
-	}
-}
-
 int main(){
 	CGraph<char,int> grafo;
 	grafo.insertNode('A');
@@ -288,14 +269,12 @@ int main(){
 	grafo.insertNode('D');
 	grafo.insertNode('E');
 	grafo.insertNode('F');
-	grafo.insertEdge('A','B',5,1);
-	grafo.insertEdge('A','C',2,1);
-	grafo.insertEdge('C','B',2,1);
-	grafo.insertEdge('B','D',3,1);
-	grafo.insertEdge('A','D',9,0);
-	grafo.insertEdge('D','E',4,1);
-	grafo.insertEdge('E','F',5,1);
-	grafo.insertEdge('F','D',5,1);
+	grafo.insertEdge('A','B',10,1);
+	grafo.insertEdge('B','C',5,1);
+	grafo.insertEdge('A','D',1,1);
+	grafo.insertEdge('D','F',1,1);
+	grafo.insertEdge('F','B',2,1);
+	grafo.deleteNode('F');
 	grafo.imprimirGrafo();
 	grafo.dijkstra('A');
 	return 0;
